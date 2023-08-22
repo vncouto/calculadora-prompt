6ab05a399df339de1cc1601d61d94dc6bbac84ca
Vinícius Couto <108415232+vncouto@users.noreply.github.com>
2023-08-22 14:31:00 -0300

    Initial commit

---
+++ calculadora/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadora
+{
+    internal class Program
+    {
+        enum Menu { Adicao = 1, Subtracao, Multiplicacao, Divisao, Potencia, Raiz, Sair }
+
+        static void Main(string[] args)
+        {
+            bool escolheuSair = false;  
+
+            while (!escolheuSair)
+            {
+                Console.WriteLine("Olá, esta é minha calculadora.");
+                Console.WriteLine("1. Soma\n2. Subtração\n3. Multiplicação\n4. Divisão\n5. Potenciação\n6. Raiz\n7. Sair");
+
+                Menu opcao = (Menu)int.Parse(Console.ReadLine());
+
+                Console.WriteLine(opcao);
+            }
+        }
+    }
+}
